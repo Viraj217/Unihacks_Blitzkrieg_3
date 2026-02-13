773782d996363c417639c21efd6f5334db08688b
Viraj Shah <virajshah177@gmail.com>
2026-02-13 21:14:51 +0530

    Added navbar

---
+++ blitzkrieg/lib/pages/bereal_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BeRealPage extends StatelessWidget {
+  const BeRealPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'BeReal',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
---
+++ blitzkrieg/lib/pages/timeline_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TimelinePage extends StatelessWidget {
+  const TimelinePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Timeline',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
